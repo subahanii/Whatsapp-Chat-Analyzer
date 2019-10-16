e417f2d8b2b8a74059b34ed20817ef86239defb8
Ghulam Mohiyuddin <subahani98@gmail.com>
2019-10-17 01:11:10 +0530

    Update chatAnalyzer.py

--- chatAnalyzer.py
+++ chatAnalyzer.py
@@ -1,14 +1,20 @@
 """
 Author : Ghulam Mohiyuddin
 What is this: This is a whatsapp group/indivisual chat analyser 
-Message: This is not a final wait for more functionality
+Note: This is not a final wait for more functionality
 """
+
+
 
 import os
 import re
 import pandas as pd
-import matplotlib as plt
-link="C:\\Users\\asus\\Downloads\\WhatsApp Chat with EE Batch 2019.txt"# FILE LOCATION
+import matplotlib.pyplot as plt
+#link="C:\\Users\\asus\\Downloads\\WhatsApp Chat with GATE CS & IT - 2020.txt"
+#link="C:\\Users\\asus\\Downloads\\WhatsApp Chat with Only information no emoji.txt"#250-
+#link="C:\\Users\\asus\\Downloads\\WhatsApp Chat with CSE YR16-20.txt"#300+
+link="C:\\Users\\asus\\Downloads\\WhatsApp Chat with Unemployed peeps.txt"
+#link="C:\\Users\\asus\\Downloads\\WhatsApp Chat with Aise hi bas.txt"
 tit=link.split("\\")
 title=tit[-1]
 title1=title[:len(title)-4:]
@@ -17,13 +23,28 @@ cht=open(link,encoding="utf8")
 ar=[]
 c=0
 c1=0
+ar1=[]
+c2=0
+f=0
 def startsWithDate(s):
-    pattern = "^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]|[0-9]):([0-9][0-9])"
+    #ReGex finding date and time
+    pattern = "^([0-2][0-9]|(3)[0-1])(\/)(([0-9])|((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]|[0-9]):([0-9][0-9])"
     result = re.match(pattern, s)
     if result:
         return True
     return False
-
+def findColon(s):
+    n=len(s)
+    c=0
+    for i in range(n):
+        if s[i]==":":
+            c+=1
+    return c
+    
+    
+    
+    
+    
 def startsWithAuthor(s):
 
     cout=0
@@ -38,47 +59,102 @@ def startsWithAuthor(s):
             break
     
     return s[st+2:l] 
-c2=0
-f=0
-while  cht.readline():
+
+
+
+while 1:
     rd=cht.readline()
-    #print(rd)
+    if not rd:break
+        
+   # print(rd)
     c1+=1
-    if startsWithDate(str(rd)):
+    if startsWithDate(rd):
+       # print(rd)
         splitLine=rd.split("-")
         dateTime=splitLine[0]
         #print(dateTime)
         date,time=dateTime.split(',')
-        authorMsg=splitLine[1].split(":")
-        #print(authorMsg)
-     
-        author= authorMsg[0]
-        msg=authorMsg[1::]
-        #print("-->",time,date,author,msg)
-        ar.append([date,time,author,msg])
         c+=1
+    
+        if findColon(splitLine[1])>0:
+            c2+=1
+            authorMsg=splitLine[1].split(":")
+            
+     
+            author= authorMsg[0][:15]+".."
+            msg=authorMsg[1::]
+            
+            ar.append([date,time,author,msg])
+        
+        else:
+            ar1.append(splitLine[1])
+            
 
 
         
         
-print(c,c1,c2)
-#print(ar)
-df=pd.DataFrame(ar,columns=["Date","Time","Author","Message"])
+print("Total msg-",c,"Total valid msg-",c2)
+
+df=pd.DataFrame(ar,columns=["Date","Time","GroupMember","Message"])
+
+l=dict(df['GroupMember'].value_counts())
+xval=[]
+yval=[]
+for x,y in l.items():
+    xval.append(x)
+    yval.append(y)
+
+def showAll():
+
+    plt.figure(figsize=(len(l)*0.25,10))
+    plt.bar(xval,yval,width=0.8)
+    
+    plt.title("Group: "+title1)
+    plt.xlabel("Group Members")
+    plt.ylabel("Number of messages")
+    plt.xticks(xval,rotation=90)
+    
+    
+showAll()
+c3=0
+def autolabel(x,y):
+    global c3
+    for i in range(len(x)):
+
+        plt.text(x[i],y[i]+5,str(y[i]),ha='center',rotation=90,color='red')
+        c3+=y[i]
+autolabel(xval,yval)
+plt.text(len(l)-len(l)//2,len(l), 'Total Active Members: '+str(len(l))+", Total Message-"+str(c3),color='red')
+
+plt.show()
+#plt.savefig('test.png')
 
 
+"""
+#print(ar[0])
 def top10Author():
-    l=df['Author'].value_counts()
+    l=df['GroupMember'].value_counts()
+   
     
     
-    plt.pyplot.title(title1)
-    plt.pyplot.xlabel('Number of messages')
-    plt.pyplot.ylabel('Group Members')
-    top10auth=l.tail(15)
-    top10auth.plot.barh()
-    plt.pyplot.figure(figsize=(100,150))
-    #mng=plt.pyplot.get_current_fig_manager()
-    #mng.window.showMaximized()
+    #plt.title(title1)
+    plt.ylabel('Number of messages')
+    plt.xlabel('Group Members')
+    top10auth=l.iloc[range(0,len(l))]
+    #top10auth.plot.line()
+    top10auth.plot.bar(legend=True,title="Group: "+title1,fontsize=8,figsize=(len(l)*0.3,5))
+    #top10auth.plot.pie()
     
+    top10auth.plot.bar()
+   
 
-top10Author()
-df
+#top10Author()
+#print(len(ar1))
+#df.loc[range(20,60)]
+#df[["Date","groupMember"]]
+#df.describe()
+#sort_values(self, by, axis=0, ascending=True
+#df.query('Date=="19/04/2019" ').sort_values("Time",ascending=True)
+#print(df.to_html())
+    
+"""
